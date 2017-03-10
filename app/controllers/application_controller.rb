c14e34256e3058d59dd3873056468cd87d2a7c93
Tristan Gruener <tristan.gruener@gmail.com>
2017-03-10 00:19:02 -0800

    work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: 'whatup'
+  end
+
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
 end
